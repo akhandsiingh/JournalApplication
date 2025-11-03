7b11f08c96ace76c69d7f76ec353d7ddf3b81676
Akhand Singh <akhandsingh4906@gmail.com>
2025-11-03 13:52:42 +0530

    Update UserController.java

--- src/main/java/com/example/AkSB/controller/UserController.java
+++ src/main/java/com/example/AkSB/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.AkSB.controller;
 
 import com.example.AkSB.JournalEntryRepository.UserRepository;
+import com.example.AkSB.api.response.WeatherResponse;
 import com.example.AkSB.entity.User;
 import com.example.AkSB.service.UserService;
+import com.example.AkSB.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,9 @@ public class UserController {
     private UserService userService;
    @Autowired
     private UserRepository userRepository;
+
+   @Autowired
+    private WeatherService weatherService;
 
    @GetMapping
    public List<User> getAllUsers(){
@@ -44,6 +49,17 @@ public class UserController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         userRepository.deleteByUserName(authentication.getName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @GetMapping("get-Weather")
+
+    public ResponseEntity<?> getWeather(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        WeatherResponse weatherResponse = weatherService.getWeather("Mumbai");
+        String greeting="";
+        if(weatherResponse!=null){
+            greeting=", Weather feels like"+weatherResponse.getCurrent().getFeelslike();
+        }
+        return new ResponseEntity<>("Hi  "+authentication.getName()+ greeting ,HttpStatus.OK);
     }
 
 
