beeaeee4e387aedfa4ea0f2873c19f3fece902b0
Akhand Singh <akhandsingh4906@gmail.com>
2025-11-03 13:52:49 +0530

    Update UserService.java

--- src/main/java/com/example/AkSB/service/UserService.java
+++ src/main/java/com/example/AkSB/service/UserService.java
@@ -2,7 +2,10 @@ package com.example.AkSB.service;
 
 import com.example.AkSB.JournalEntryRepository.UserRepository;
 import com.example.AkSB.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,19 +16,28 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
     private static final PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+    private static final Logger logger= LoggerFactory.getLogger(UserService.class);
+
     public void saveUser(User user){
        userRepository.save(user);
     }
 
-    public void saveNewUser(User user){
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER")) ;
-        userRepository.save(user);
+    public boolean saveNewUser(User user){
+       try {
+           user.setPassword(passwordEncoder.encode(user.getPassword()));
+           user.setRoles(Arrays.asList("USER")) ;
+           userRepository.save(user);
+           return true;
+       }catch(Exception e){
+           log.info("Exception Occured");
+           return false;
+       }
     }
     public void saveAdmin(User user){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
