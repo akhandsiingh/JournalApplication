1c0fab6e506c30ff8996c43e9d31e22af717ee21
Akhand Singh <akhandsingh4906@gmail.com>
2025-11-03 13:52:45 +0530

    Update JournalEntryService.java

--- src/main/java/com/example/AkSB/service/JournalEntryService.java
+++ src/main/java/com/example/AkSB/service/JournalEntryService.java
@@ -3,7 +3,10 @@ package com.example.AkSB.service;
 import com.example.AkSB.JournalEntryRepository.JournalEntryRepository;
 import com.example.AkSB.entity.JournalEntry;
 import com.example.AkSB.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +17,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
     @Autowired
     private  UserService userService;
+    private static final Logger logger= LoggerFactory.getLogger(JournalEntryService.class);
 
     @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
@@ -29,7 +34,7 @@ public class JournalEntryService {
          user.getJournalEntries().add(saved);
          userService.saveUser(user);
      }catch (Exception e){
-         System.out.println(e);
+         logger.info("exception");
          throw new RuntimeException("An error Occured while saving entry"+e);
      }
     }
@@ -50,7 +55,7 @@ public class JournalEntryService {
              journalEntryRepository.deleteById(id);
          }
      }catch(Exception e){
-         System.out.println(e);
+       log.error("Exception",e);
          throw new RuntimeException("An error Occured"+e);
      }
         return removed;
