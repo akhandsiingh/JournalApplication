5f54a1046f5d9b1b5437821797caa647d43c5f7f
Akhand Singh <akhandsingh4906@gmail.com>
2025-11-03 22:56:55 +0530

    Update WeatherService.java

--- src/main/java/com/example/AkSB/service/WeatherService.java
+++ src/main/java/com/example/AkSB/service/WeatherService.java
@@ -5,15 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
     public static final String apiKey="";
 
     @Autowired
     private RestTemplate restTemplate;
-
     public static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
      public WeatherResponse getWeather(String city){
          String finalAPI=API.replace("CITY",city).replace("API_KEY",apiKey);
