99ba59ecf5bb2e241ced56780ee7b792b137a231
Akhand Singh <akhandsingh4906@gmail.com>
2025-11-03 22:57:27 +0530

    Update JournalEntry.java

--- src/main/java/com/example/AkSB/entity/JournalEntry.java
+++ src/main/java/com/example/AkSB/entity/JournalEntry.java
@@ -1,12 +1,12 @@
 package com.example.AkSB.entity;
 
+import com.example.AkSB.enums.Sentiment;
 import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Document(collection = "journal_entries")
 //@Getter
@@ -21,6 +21,6 @@ public class JournalEntry {
 
     private String content;
     private LocalDateTime date;
-
+    private Sentiment setiment;
 
 }
