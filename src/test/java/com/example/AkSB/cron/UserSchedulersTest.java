a11d2022fdbc56a72d9a8414f048990d9c7cb4c6
Akhand Singh <akhandsingh4906@gmail.com>
2025-11-04 03:09:47 +0530

    JUnit

---
+++ src/test/java/com/example/AkSB/cron/UserSchedulersTest.java
@@ -0,0 +1,20 @@
+package com.example.AkSB.cron;
+
+
+import com.example.AkSB.scheduler.UserScheduler;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserSchedulersTest {
+
+    @Autowired
+    private UserScheduler userScheduler;
+
+
+    @Test
+    public void testFetchUsersAndSendSaMail(){
+        userScheduler.fetchUsersAndSendSaMail();
+    }
+}
