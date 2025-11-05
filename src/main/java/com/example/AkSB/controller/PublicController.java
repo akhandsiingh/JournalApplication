fb2e815fa47994eb33a843ca3132fa9a0fb50aa7
Akhand Singh <akhandsingh4906@gmail.com>
2025-11-06 01:21:57 +0530

    Swagger Impl

--- src/main/java/com/example/AkSB/controller/PublicController.java
+++ src/main/java/com/example/AkSB/controller/PublicController.java
@@ -1,9 +1,15 @@
 package com.example.AkSB.controller;
 
+import com.example.AkSB.dto.UserDTO;
 import com.example.AkSB.entity.User;
 import com.example.AkSB.service.UserDetailsServiceImpl;
 import com.example.AkSB.service.UserService;
 import com.example.AkSB.utilils.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,7 +22,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/public")
 @Slf4j
+@Tag(name = "Public APIs")
 public class PublicController {
+
     @Autowired
     private UserService userService;
     @Autowired
@@ -26,26 +34,37 @@ public class PublicController {
     @Autowired
     private JwtUtil jwtUtil;
 
-
     @GetMapping("/health-check")
-    public String healthCheck(){
+    @Operation(summary = "Health Check", description = "Verify if the service is running")
+    public String healthCheck() {
         return "ok";
     }
 
     @PostMapping("/signup")
-    public void signup(@RequestBody User user){
-        userService.saveNewUser(user);
+    @Operation(summary = "User Signup", description = "Register a new user with basic details")
+    public void signup(@RequestBody UserDTO user) {
+        User newUser = new User();
+        newUser.setEmail(user.getEmail());
+        newUser.setUserName(user.getUserName());
+        newUser.setPassword(user.getPassword());
+        newUser.setSentimentAnalysis(user.isSentimentAnalysis());
+        userService.saveNewUser(newUser);
     }
+
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user){
-      try {
-          authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
-          UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
-          String jwt = jwtUtil.generateToken(userDetails.getUsername());
-          return new ResponseEntity<>(jwt, HttpStatus.OK);
-      }catch(Exception e){
-          log.error("Exception while Authenticating token ",e);
-          return new ResponseEntity<>("Incorrect Username or Password",HttpStatus.BAD_REQUEST);
-      }
+    @Operation(summary = "User Login", description = "Authenticate user and generate JWT token",
+            requestBody = @RequestBody(required = true,
+                    content = @Content(schema = @Schema(
+                            example = "{ \"userName\": \"exampleUser\", \"password\": \"password123\" }"))))
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception while Authenticating token ", e);
+            return new ResponseEntity<>("Incorrect Username or Password", HttpStatus.BAD_REQUEST);
+        }
     }
 }
