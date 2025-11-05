d17f810c4e54c07313b66748218360039a901b58
Akhand Singh <akhandsingh4906@gmail.com>
2025-11-06 01:22:04 +0530

    Swagger Impl

--- src/main/java/com/example/AkSB/controller/UserController.java
+++ src/main/java/com/example/AkSB/controller/UserController.java
@@ -5,6 +5,11 @@ import com.example.AkSB.api.response.WeatherResponse;
 import com.example.AkSB.entity.User;
 import com.example.AkSB.service.UserService;
 import com.example.AkSB.service.WeatherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,49 +21,66 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User APIs", description = "Read, Update and Delete User")
 public class UserController {
-   @Autowired
-    private UserService userService;
-   @Autowired
-    private UserRepository userRepository;
 
-   @Autowired
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
     private WeatherService weatherService;
 
-   @GetMapping
-   public List<User> getAllUsers(){
-       return userService.getAll();
-   }
-   @PostMapping
-    public void createUser(@RequestBody User user){
-       userService.saveNewUser(user);
-   }
-   @PutMapping
-    public ResponseEntity<?> updateUser(@RequestBody User user){
-       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-       String userName= authentication.getName();
-       User userInDb=userService.findByUserName(userName);
-       userInDb.setUserName(user.getUserName());
-       userInDb.setPassword(user.getPassword());
-       userService.saveUser(userInDb);
-       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-   }
+    @GetMapping
+    @Operation(summary = "Get all users", description = "Fetch list of all registered users")
+    public List<User> getAllUsers() {
+        return userService.getAll();
+    }
+
+    @PostMapping
+    @Operation(summary = "Create new user", description = "Register a new user",
+            requestBody = @RequestBody(required = true,
+                    content = @Content(schema = @Schema(
+                            example = "{ \"userName\": \"exampleUser\", \"email\": \"example@gmail.com\", \"password\": \"password123\" }"))))
+    public void createUser(@RequestBody User user) {
+        userService.saveNewUser(user);
+    }
+
+    @PutMapping
+    @Operation(summary = "Update user", description = "Update the current logged-in user's details",
+            requestBody = @RequestBody(required = true,
+                    content = @Content(schema = @Schema(
+                            example = "{ \"userName\": \"updatedUser\", \"password\": \"newPassword\" }"))))
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        User userInDb = userService.findByUserName(userName);
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+        userService.saveUser(userInDb);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
     @DeleteMapping("/user")
-    public ResponseEntity<?>deleteUserById(@RequestBody User user){
+    @Operation(summary = "Delete user", description = "Delete the current logged-in user",
+            requestBody = @RequestBody(required = true,
+                    content = @Content(schema = @Schema(
+                            example = "{ \"userName\": \"exampleUser\" }"))))
+    public ResponseEntity<?> deleteUserById(@RequestBody User user) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         userRepository.deleteByUserName(authentication.getName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-    @GetMapping("get-Weather")
 
-    public ResponseEntity<?> getWeather(){
+    @GetMapping("get-Weather")
+    @Operation(summary = "Get current weather", description = "Fetch current weather info for Mumbai for the logged-in user")
+    public ResponseEntity<?> getWeather() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         WeatherResponse weatherResponse = weatherService.getWeather("Mumbai");
-        String greeting="";
-        if(weatherResponse!=null){
-            greeting=", Weather feels like"+weatherResponse.getCurrent().getFeelslike();
+        String greeting = "";
+        if (weatherResponse != null) {
+            greeting = ", Weather feels like " + weatherResponse.getCurrent().getFeelslike();
         }
-        return new ResponseEntity<>("Hi  "+authentication.getName()+ greeting ,HttpStatus.OK);
+        return new ResponseEntity<>("Hi " + authentication.getName() + greeting, HttpStatus.OK);
     }
 }
