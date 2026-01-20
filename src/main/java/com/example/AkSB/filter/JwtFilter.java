c22ec7e8f3cb6bb619fa2efe44ca59671f0e8f3f
Akhand Singh <akhandsingh4906@gmail.com>
2026-01-20 13:45:14 +0530

    Update JwtFilter.java

--- src/main/java/com/example/AkSB/filter/JwtFilter.java
+++ src/main/java/com/example/AkSB/filter/JwtFilter.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
-
+@Component
 public class JwtFilter  extends OncePerRequestFilter{
     @Autowired
     private UserDetailsService userDetailsService;
