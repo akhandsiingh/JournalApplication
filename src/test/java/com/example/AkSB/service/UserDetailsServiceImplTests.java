19757ec6e18f2fcf17fe4dd9ed72619f68fe2cf1
Akhand Singh <akhandsingh4906@gmail.com>
2026-01-22 18:54:44 +0530

    Update UserDetailsServiceImplTests.java

--- src/test/java/com/example/AkSB/service/UserDetailsServiceImplTests.java
+++ src/test/java/com/example/AkSB/service/UserDetailsServiceImplTests.java
@@ -33,7 +33,7 @@ public class UserDetailsServiceImplTests {
     @Disabled
     @Test
     void loadUserByUsernameTest(){
-        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("").roles(new ArrayList<>()).build());
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("").password("").roles(new ArrayList<>()).build());
         UserDetails user = userDetailsService.loadUserByUsername("ram");
         Assertions.assertNotNull(user);
     }
